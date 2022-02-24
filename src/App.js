7b19d18e6483e2256f95a8f0fdfc807afaa4cc87
Bonsi <amonselltapia@gmail.com>
2022-02-24 16:57:45 +0100

    header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import './App.css';
-// import Loggin from './components/loggin/loggin';
-import Header from './components/header/Header';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Loggin from "./components/loggin/loggin";
+import Header from "./components/header/Header";
 
 function App() {
   return (
-    <div>
-      {/* <Loggin/> */}
-      <Header/>
-    </div>
-  )
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Routes>{/* <Loggin /> */}</Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,33 @@
-import {Link} from 'react-router-dom';
-import './Header.css'
-const Header = ()=>{
-    return(
-        <nav className='headerContainer'>
-            <img alt="logo"src="https://res.cloudinary.com/ducxt7zb3/image/upload/v1645532143/tuentiTransparente_tozqdb.png"></img>
-            {/* <Link to="/">Home</Link> */}
-        </nav>
-    )
-}
+import { Link } from "react-router-dom";
+import { BiSearchAlt2 } from "react-icons/bi";
+import { ImArrowUp } from "react-icons/im";
+import "./Header.css";
+const Header = () => {
+  return (
+    <nav className="headerContainer">
+      <div className="headerMenu">
+        <img
+          alt="logo"
+          src="https://res.cloudinary.com/ducxt7zb3/image/upload/v1645532143/tuentiTransparente_tozqdb.png"
+        ></img>
+        <Link to="/">Home</Link>
+        <Link to="/profile">Profile</Link>
+        <Link to="/messages">Messages</Link>
+        <Link to="/people">People</Link>
+        <Link to="/videos">Videos</Link>
+        <div className="headerSearch">
+          <input type="search" placeholder="Search"></input>
+          <button className="glass">
+            <BiSearchAlt2 />
+          </button>
+        </div>
+        <button className="upload">
+          Upload Photos <ImArrowUp />
+        </button>
+      </div>
+      <p className="headerMyAccount">My account</p>
+    </nav>
+  );
+};
 
 export default Header;
